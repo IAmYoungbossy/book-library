e44af9de73e4115ec69793e54307edc0e92daf19
Letam Bossman Barinua <letambarinua@gmail.com>
2022-07-03 13:41:03 +0100

    Cache the DOM elements into variables in JS file

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+const bookTitle = document.querySelector('#book-title');
+const bookAuthor = document.querySelector('#book-author');
+const bookPages = document.querySelector('#book-pages');
+const addBook = document.querySelector('#add-book');
+const body = document.querySelector('body');
+const container = document.querySelector('.card-container');
+const check = document.querySelector('#read');
+
+let bookLibrary = [];
